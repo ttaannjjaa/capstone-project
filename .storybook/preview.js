c903b110bebfd2aa228d76e9835a56137b878b0d
Tanja Süß <ajnat2268@gmail.com>
2022-03-06 21:57:17 +0100

    initial commit

---
+++ .storybook/preview.js
@@ -0,0 +1,22 @@
+import GlobalStyles from "../src/GlobalStyles.js"
+
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+export const decorators = [
+(Story) => (
+  <>
+  <GlobalStyles />
+  <Story />
+  </>
+)
+
+]
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from './GlobalStyles.js';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
