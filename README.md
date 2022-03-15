fac498b0108e180f0fc5e2d438a4f0d97cb32167
Tanja Süß <ajnat2268@gmail.com>
2022-03-15 19:05:43 +0100

    Add search function

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 ## Capstone Project of the IT bootcamp at neuefische
 
-CatYummy allows you to track your cat's decisions about its food. After adding the food, your cat likes or not, CatYummy will help your memory out when you will stand in front of a long row with cat food products of the supermarket or pet shop next time, trying to remember which food your cat has already tested...and didn' t like it. It will save your money, helps to reduce waste and makes your cat happier.
+CatYummy allows you to track your cat's decisions about its food. After adding the food, your cat likes or not, CatYummy will help your memory out next time when you will stand in front of a long row with cat food products in the supermarket or pet, trying to remember which food your cat has already tested...and didn' t like it. CatYummy will save your money, helps to reduce waste and makes your cat happier.
 
 CatYummy is under construction.
-So far there are three pages you can visit. A good start is the form to add another listcontent according to how your cat accepted the last food you gave to it. You can now see the new listitem on the according page. The data are stored in the local storage of your browser, so the app will (most probably) not loose them, when you reload or re-open the app.
+So far there are three pages you can visit. A good start is the form to add another listcontent according to how your cat accepted the last food you gave to it. You can now see the new listitem on the according page. The data are stored in the local storage of your browser, so the app will (most probably) not loose them, when you reload or re-open the app. The newest feature is that i added a searchbar in the list of not liked food, so you can search for catfood (you have already added) with its name, taste or its preparation without scrolling along the whole list.
 
 ### Deployment
 
@@ -16,7 +16,6 @@ You can have a look at the last update of CatYummy on Vercel: [DemoVersion](http
 - React v17.02
 - React Router 6.2.2
 - React Testing Library / Jest
-- Immer (hook: useImmer)
 - Storybook
 - styled components
 - Javascript
--- src/pages/UnLikedFoodPage.js
+++ src/pages/UnLikedFoodPage.js
@@ -3,39 +3,23 @@ import Card from '../components/Card.js';
 import Searchbar from '../components/Searchbar.js';
 import Navigation from '../components/Navigation.js';
 import { useState } from 'react';
-//import { useImmer } from 'use-immer';
 
 export default function UnLikedFoodPage({ storageData }) {
-  const UnLikedData = storageData?.filter(data => data.foodJudge === 'unliked');
+  const unLikedData = storageData?.filter(data => data.foodJudge === 'unliked');
 
   const [userInput, setUserInput] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
 
   function handleUserInput(event) {
-    event.stopPropagation();
     event.preventDefault();
-    const input = event.target.value.trim().toLowerCase();
-    console.log('nr1', input);
+    const input = event.target.value;
     setUserInput(input);
-    searchFunction(input);
-    console.log('searchfunction wurde aufgerufen');
-    //event.target.reset();
   }
 
-  function searchFunction(eingabe) {
-    const dataFilteredByFoodName = UnLikedData.filter(data =>
-      data.foodName.toLowerCase().includes(eingabe)
-    );
-    // const dataFilteredByFoodTaste = UnLikedData.filter(data =>
-    //   data.foodTaste.toLowerCase().includes(input)
-    // );
-    // const dataFilteredByFoodStyle = UnLikedData.filter(data =>
-    //   data.foodStyle.toLowerCase().includes(input)
-    // );
-    setSearchResults(dataFilteredByFoodName);
-    // updateSearchResults(dataFilteredByFoodTaste);
-    // updateSearchResults(dataFilteredByFoodStyle);
-  }
+  const filteredUnlikedData = unLikedData.filter(data =>
+    Object.values(data).some(val =>
+      val.toLowerCase().includes(userInput.trim().toLowerCase())
+    )
+  );
 
   return (
     <UnLikedFoodPageStyled>
@@ -44,7 +28,7 @@ export default function UnLikedFoodPage({ storageData }) {
         <Searchbar handleUserInput={handleUserInput} userInput={userInput} />
       </Header>
       <main>
-        {UnLikedData.length === 0 && (
+        {unLikedData.length === 0 && (
           <p>
             Du hast hier noch keine Listeneinträge. Listeneinträge erscheinen,
             wenn das Formular ausgefüllt und gespeichert wird.
@@ -56,7 +40,7 @@ export default function UnLikedFoodPage({ storageData }) {
             aria-describedby="list of cards about catfood that your cat does not like"
             role="list"
           >
-            {UnLikedData.map((data, index) => (
+            {unLikedData.map((data, index) => (
               <li key={index}>
                 <Card
                   foodName={data.foodName}
@@ -68,31 +52,30 @@ export default function UnLikedFoodPage({ storageData }) {
             ))}
           </ListStyle>
         )}
-        {userInput.length > 0 &&
-          searchResults?.length ===
-            0(
-              <p>
-                Entweder mag deine Katze das Futter oder es ist hier noch nicht
-                als Listeneintrag vorhanden.
-              </p>
-            )}
-        {userInput.length > 0 && searchResults?.length > 0}{' '}
-        <ListStyle
-          data-testid="unlikedlist"
-          aria-describedby="list of cards about catfood that your cat does not like"
-          role="list"
-        >
-          {searchResults.map((data, index) => (
-            <li key={index}>
-              <Card
-                foodName={data.foodName}
-                foodTaste={data.foodTaste}
-                foodStyle={data.foodStyle}
-                foodJudge={data.foodJudge}
-              />
-            </li>
-          ))}
-        </ListStyle>
+        {userInput.length > 0 && filteredUnlikedData.length === 0 && (
+          <p>
+            Entweder mag deine Katze das Futter oder es ist hier noch nicht als
+            Listeneintrag vorhanden.
+          </p>
+        )}
+        {userInput.length > 0 && filteredUnlikedData.length > 0 && (
+          <ListStyle
+            data-testid="unlikedlist"
+            aria-describedby="list of cards about catfood that your cat does not like"
+            role="list"
+          >
+            {filteredUnlikedData.map((data, index) => (
+              <li key={index}>
+                <Card
+                  foodName={data.foodName}
+                  foodTaste={data.foodTaste}
+                  foodStyle={data.foodStyle}
+                  foodJudge={data.foodJudge}
+                />
+              </li>
+            ))}
+          </ListStyle>
+        )}
       </main>
       <footer>
         <Navigation />
